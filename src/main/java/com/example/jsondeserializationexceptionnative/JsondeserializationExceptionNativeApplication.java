af662b58a9374bd1cfbc177ea06686e88b979f8e
tuckeremulls <87487801+tuckeremulls@users.noreply.github.com>
2023-03-29 14:11:01 -0400

    Update JsondeserializationExceptionNativeApplication.java

--- src/main/java/com/example/jsondeserializationexceptionnative/JsondeserializationExceptionNativeApplication.java
+++ src/main/java/com/example/jsondeserializationexceptionnative/JsondeserializationExceptionNativeApplication.java
@@ -1,9 +1,7 @@
 package com.example.jsondeserializationexceptionnative;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JsondeserializationExceptionNativeApplication {
